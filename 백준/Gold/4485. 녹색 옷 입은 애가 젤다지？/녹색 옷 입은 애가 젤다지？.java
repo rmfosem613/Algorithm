304b49a6d76f1f9832ab895d7a289f194ed9212e
Mi-Gyeong <51354302+rmfosem613@users.noreply.github.com>
2024-11-02 23:17:01 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 200 ms, Memory: 20348 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,82 +1,73 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
-    private static final int INF = Integer.MAX_VALUE; // 무한대
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
+	static int N;
+	static int[][] map;
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { 1, 0, -1, 0 };
+	static int result;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
 
-        int result = 1;
-        StringTokenizer st;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true) {
-            int V = Integer.parseInt(in.readLine());
-            if (V == 0)
-                break;
+		int cnt = 1;
+		
+		while (true) {
+			N = Integer.parseInt(br.readLine());
 
-            int[][] adjMatrix = new int[V][V];
+			if (N == 0)
+				break;
 
-            for (int i = 0; i < V; i++) {
-                st = new StringTokenizer(in.readLine(), " ");
-                for (int j = 0; j < V; j++) {
-                    adjMatrix[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
+			map = new int[N][N];
 
-            // 다익스트라 알고리즘에 필요한 것들
-            int[][] distance = new int[V][V];
-            boolean[][] visited = new boolean[V][V];
-            for (int i = 0; i < V; i++) {
-                Arrays.fill(distance[i], INF);
-            }
+			result = Integer.MAX_VALUE;
 
-            // 출발 지점 설정
-            distance[0][0] = adjMatrix[0][0];
+			for (int i = 0; i < N; i++) {
+				String split[] = br.readLine().split(" ");
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(split[j]);
+				}
+			}
 
-            while (true) {
-                int min = INF;
-                int currentX = -1, currentY = -1;
+			bfs(0, 0);
 
-                // 방문하지 않은 노드 중에서 최소 거리를 가진 노드를 선택
-                for (int i = 0; i < V; i++) {
-                    for (int j = 0; j < V; j++) {
-                        if (!visited[i][j] && distance[i][j] < min) {
-                            min = distance[i][j];
-                            currentX = i;
-                            currentY = j;
-                        }
-                    }
-                }
+			System.out.println("Problem " + (cnt++) + ": " + result);
+		}
 
-                // 모든 노드를 방문했다면 종료
-                if (currentX == V-1 && currentY == V-1) break;
+	}
 
-                // 현재 노드를 방문 처리
-                visited[currentX][currentY] = true;
+	private static void bfs(int x, int y) {
+		Queue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+		queue.add(new int[] { 0, 0, map[0][0] });
+		boolean[][] visited = new boolean[N][N];
+		visited[0][0] = true;
 
-                // 현재 노드에서 인접한 노드의 최소 비용 갱신
-                for (int d = 0; d < 4; d++) {
-                    int nextX = currentX + dx[d];
-                    int nextY = currentY + dy[d];
+		while (!queue.isEmpty()) {
+			int[] temp = queue.poll();
+			int cx = temp[0];
+			int cy = temp[1];
+			int cd = temp[2];
 
-                    if (nextX >= 0 && nextX < V && nextY >= 0 && nextY < V) {
-                        if (!visited[nextX][nextY] 
-                                && distance[nextX][nextY] > distance[currentX][currentY] + adjMatrix[nextX][nextY]) {
-                            distance[nextX][nextY] = distance[currentX][currentY] + adjMatrix[nextX][nextY];
-                        }
-                    }
-                }
-            }
+			if (cx == N - 1 && cy == N - 1) {
+				result = Math.min(result, cd);
+				return;
+			}
 
-            // 결과 출력
-            System.out.print("Problem " + result + ": ");
-            System.out.println(distance[V - 1][V - 1]);
-            result++;
-        }
-    }
+			for (int i = 0; i < 4; i++) {
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
+
+				if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
+					visited[nx][ny] = true;
+					queue.offer(new int[] { nx, ny, cd + map[nx][ny] });
+				}
+			}
+		}
+
+	}
 }
