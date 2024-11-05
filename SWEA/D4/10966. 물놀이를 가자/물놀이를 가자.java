7c9be9f9c3d85d8455619d75b57e61b460d62a78
Mi-Gyeong <51354302+rmfosem613@users.noreply.github.com>
2024-11-05 21:01:06 +0900

    [D4] Title: 물놀이를 가자, Time: 1,046 ms, Memory: 118,068 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -1,82 +1,125 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Solution {
-	
-	static class L{
-		int r;
-		int c;
-		int distance;
-		public L(int r, int c, int distance) {
-			this.r = r;
-			this.c = c;
-			this.distance = distance;
-		}
-	}
-	static int [] dr=  {-1,0,1, 0};
-    static int [] dc = { 0,1,0,-1};
-    static int T, N, M;
-    static int sum;
-    static int[][] grid;
-    static int Wa,La;
-	static  Queue<L>  water;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-        for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            water=new LinkedList<>();
-            grid=new int[N][M];
-            // dijkstra 응용
-            for (int i = 0; i < N; i++) {
-				Arrays.fill(grid[i], 10000000);
+	static int N, M;
+	static char map[][];
+
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { 1, 0, -1, 0 };
+
+	static Queue<int[]> water;
+	static int[][] distance;
+
+	static int INF = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int test_case = 1; test_case < T + 1; test_case++) {
+            sb.append("#"+test_case+" ");
+			String[] split = br.readLine().split(" ");
+			N = Integer.parseInt(split[0]);
+			M = Integer.parseInt(split[1]);
+			map = new char[N][M];
+			distance = new int[N][M];
+
+			// 다익스트라
+			for (int[] d : distance) {
+				Arrays.fill(d, INF);
 			}
-            
-            for(int i = 0; i < N; i++) {
-            	String ss=br.readLine();
-            	char[] cs=ss.toCharArray();
-                for(int j = 0; j < M; j++) {
-                	if(cs[j]=='W') {
-                		water.offer(new L(i, j,0));
-                		grid[i][j]=0; //시작을 0으로 
-                	}
-                }
-            }
-            sum=0;
-            bfs();
-            for (int i = 0; i < N; i++) {
+
+			water = new ArrayDeque<int[]>();
+
+			// map 입력받기
+			for (int i = 0; i < N; i++) {
+				String split2 = br.readLine();
 				for (int j = 0; j < M; j++) {
-					sum+=grid[i][j];
+					map[i][j] = split2.charAt(j);
+
+					if (map[i][j] == 'W') {
+						water.offer(new int[] { i, j });
+						distance[i][j] = 0;
+//						list.add(new int[] { i, j });
+					}
 				}
-			}            
-            System.out.println("#"+t+" "+sum);
-        }
-    }
-    static void bfs() {
-        while(!water.isEmpty()) {
-            L cur= water.poll();
-            int r = cur.r;
-            int c = cur.c;
-            int dis=cur.distance;
-            for(int d = 0; d < 4; d++) {
-                int nr = r + dr[d];
-                int nc = c + dc[d];
-                if(!check(nr,nc)) continue;
-                if(grid[nr][nc]>dis+1) {  // 같은 레벨 x, 항상 감소하는것만
-                	water.offer(new L (nr,nc, dis+1));
-                	grid[nr][nc]=dis+1; //시작을 0으로 
-                }
-            }
-        }
-    }
-    static boolean check(int r, int c) {
-        return r >= 0 && c >= 0 && r< N && c< M;
-    }
+			}
+
+			int sum = 0;
+			bfs();
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					sum += distance[i][j];
+//					System.out.print(distance[i][j] + " ");
+				}
+//				System.out.println();
+			}
+			sb.append(sum + " \n");
+		}
+		System.out.println(sb);
+	}
+
+	private static void bfs() {
+		while (!water.isEmpty()) {
+			int[] temp = water.poll();
+			int cx = temp[0];
+			int cy = temp[1];
+
+			for (int i = 0; i < 4; i++) {
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
+
+				if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+					
+					if(distance[nx][ny] > distance[cx][cy] + 1 && map[nx][ny] == 'L') {
+						distance[nx][ny] = distance[cx][cy] + 1;
+						water.offer(new int[] {nx,ny});
+					}
+				}
+			}
+		}
+
+	}
+
+//	private static int bfs(int x, int y) {
+//		Queue<int[]> queue = new ArrayDeque<>();
+//		queue.offer(new int[] { x, y, 0 });
+//
+//		boolean[][] visited = new boolean[N][M];
+//		visited[x][y] = true;
+//
+//		int ans = Integer.MAX_VALUE;
+//		while (!queue.isEmpty()) {
+//			int[] temp = queue.poll();
+//			int cx = temp[0];
+//			int cy = temp[1];
+//			int cd = temp[2];
+//
+//			if (map[cx][cy] == 'W') {
+//				ans = Math.min(ans, cd);
+//				return ans;
+//			}
+//
+//			for (int i = 0; i < 4; i++) {
+//				int nx = cx + dx[i];
+//				int ny = cy + dy[i];
+//
+//				if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny]) {
+//					visited[nx][ny] = true;
+//					queue.offer(new int[] { nx, ny, cd + 1 });
+//				}
+//			}
+//
+//		}
+//		return 0;
+//	}
 }
